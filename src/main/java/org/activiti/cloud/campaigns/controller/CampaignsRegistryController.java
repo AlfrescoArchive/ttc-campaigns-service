5f901f0bad1ebc25f00361112a5d1bd772898adc
Ryan Dawson <ryan.dawson@alfresco.com>
2018-08-28 15:17:58 +0100

    use v1 prefix

--- src/main/java/org/activiti/cloud/campaigns/controller/CampaignsRegistryController.java
+++ src/main/java/org/activiti/cloud/campaigns/controller/CampaignsRegistryController.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping(path = "/v1")
 @RestController
 public class CampaignsRegistryController {
 
